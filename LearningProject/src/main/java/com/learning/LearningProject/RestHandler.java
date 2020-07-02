25ccce982ee10785ee377a8c66b1ebdb74147a0f
Ravindra Kumar <ravi.loveyouall@gmail.com>
2020-07-03 00:59:36 +0530

    added emp DTO

--- LearningProject/src/main/java/com/learning/LearningProject/RestHandler.java
+++ LearningProject/src/main/java/com/learning/LearningProject/RestHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class RestHandler {
@@ -12,6 +13,9 @@ public class RestHandler {
 	public ResponseEntity<?> getResponse() {
 		
 		System.out.println("Received GET Request");
+		
+		RestTemplate restTemplate = new RestTemplate();
+		Employee emp = restTemplate.getForObject("http://localhost:8082/anotherService", Employee.class);
 		
 		return ResponseEntity.status(HttpStatus.OK)
 				.header("Param 1", "Value 1")
